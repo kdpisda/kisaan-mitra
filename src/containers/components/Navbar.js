c88f3e36681b6f6e3ea566e8fde2e4af4519afd9
Kuldeep Pisda <pisdak79@gmail.com>
2019-03-02 11:25:30 +0530

    Update: changed navbar

--- src/containers/components/Navbar.js
+++ src/containers/components/Navbar.js
@@ -22,28 +22,6 @@ export default class Navbar extends Component{
                         <a className="navbar-item">
                             Documentation
                         </a>
-
-                        <div className="navbar-item has-dropdown is-hoverable">
-                            <a className="navbar-link">
-                                More
-                            </a>
-
-                            <div className="navbar-dropdown">
-                                <a className="navbar-item">
-                                    About
-                                </a>
-                                <a className="navbar-item">
-                                    Jobs
-                                </a>
-                                <a className="navbar-item">
-                                    Contact
-                                </a>
-                                <hr className="navbar-divider" />
-                                <a className="navbar-item">
-                                    Report an issue
-                                </a>
-                            </div>
-                        </div>
                     </div>
 
                     <div className="navbar-end">
