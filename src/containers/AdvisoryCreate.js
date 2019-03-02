e31e797b9084dbe43018ed71277d552e7fb0914b
shreyashrivastava02 <shreyashrivastava02@yahoo.com>
2019-03-02 18:02:08 +0530

    update: added screens of advisory dashboard

--- src/containers/AdvisoryCreate.js
+++ src/containers/AdvisoryCreate.js
@@ -5,12 +5,108 @@ import Navbar from "./components/Navbar";
 @inject("store")
 @observer
 export default class AdvisoryCreate extends Component {
-    render() {
-        const { ui } = this.props.store;
-        return (
-            <Fragment>
-                <Navbar/>
-            </Fragment>
-        );
-    }
+  render() {
+    const { ui } = this.props.store;
+    return (
+      <Fragment>
+        <Navbar />
+        <div className="hero is-fullheight">
+          <div className="hero-body has -text-centred">
+            <div className="container">
+              <div className="field is-horizontal">
+                <div className="field-label is-normal">
+                  <label className="label">Title</label>
+                </div>
+                <div className="field-body">
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="Text input"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="field is-horizontal">
+                <div className="field-label is-normal">
+                  <label className="label">Crops</label>
+                </div>
+                <div className="field-body">
+                    <div class="field">
+    
+                        <label class="checkbox">
+                            <input type="checkbox"/>
+                            &nbsp; Crop 1
+                        </label>
+                    </div>
+                    <div class="field">
+    
+                        <label class="checkbox">
+                            <input type="checkbox"/>
+                            &nbsp; Crop 1
+                        </label>
+                    </div>
+                    <div class="field">
+    
+                        <label class="checkbox">
+                            <input type="checkbox"/>
+                            &nbsp; Crop 1
+                        </label>
+                    </div>
+                    <div class="field">
+    
+                        <label class="checkbox">
+                            <input type="checkbox"/>
+                            &nbsp; Crop 1
+                        </label>
+                    </div>
+                    <div class="field">
+    
+                        <label class="checkbox">
+                            <input type="checkbox"/>
+                            &nbsp; Crop 1
+                        </label>
+                    </div>
+                    <div class="field">
+    
+                        <label class="checkbox">
+                            <input type="checkbox"/>
+                            &nbsp; Crop 1
+                        </label>
+                    </div>
+                    <div class="field">
+    
+                        <label class="checkbox">
+                            <input type="checkbox"/>
+                            &nbsp; Crop 1
+                        </label>
+                    </div>
+                    
+                </div>
+              </div>
+
+              <div className="field is-horizontal">
+                <div className="field-label is-normal">
+                  <label className="label">Details</label>
+                </div>
+                <div className="field-body">
+                  <div className="field">
+                    <div className="control">
+                      <textarea
+                        className="textarea"
+                        placeholder="Enter the details."
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
--- src/containers/AdvisoryDashboard.js
+++ src/containers/AdvisoryDashboard.js
@@ -10,6 +10,112 @@ export default class AdvisoryDashbobard extends Component {
         return (
             <Fragment>
                 <Navbar/>
+                <section className="hero">
+                    <div className="hero-body">
+                        <div className="container">
+                        <h1 className="title">
+                            Hello Name 
+                        </h1>
+                        <h2 className="subtitle">
+                            You have been doing great job.Keep motivating.Keep advising.
+                        </h2>
+                        </div>
+                    </div>
+                </section>
+                <div className="container">
+                    <div className="columns">
+                        <div className="column">
+                            <div className="card">
+                                <div className="card-image">
+                                    <figure className="image is-4by3">
+                                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                                    </figure>
+                                </div>
+                                <div className="card-content">
+                                    
+                                        <p className="title is-4">Title</p>
+                                        <p> Description </p>
+                                        
+                                </div>
+                                    
+
+                                <footer class="card-footer">
+                                    
+                                    <a href="#" class="card-footer-item">Edit/View</a>
+                                    
+                                </footer>
+
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <div className="card-image">
+                                    <figure className="image is-4by3">
+                                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                                    </figure>
+                                </div>
+                                <div className="card-content">
+                                    
+                                        <p className="title is-4">Title</p>
+                                        <p> Description </p>
+                                        
+                                </div>
+                                    
+
+                                <footer class="card-footer">
+                                    
+                                    <a href="#" class="card-footer-item">Edit/View</a>
+                                    
+                                </footer>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <div className="card-image">
+                                    <figure className="image is-4by3">
+                                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                                    </figure>
+                                </div>
+                                <div className="card-content">
+                                    
+                                        <p className="title is-4">Title</p>
+                                        <p> Description </p>
+                                        
+                                </div>
+                                    
+
+                                <footer class="card-footer">
+                                    
+                                    <a href="#" class="card-footer-item">Edit/View</a>
+                                    
+                                </footer>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <div className="card-image">
+                                    <figure className="image is-4by3">
+                                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                                    </figure>
+                                </div>
+                                <div className="card-content">
+                                    
+                                        <p className="title is-4">Title</p>
+                                        <p> Description </p>
+                                        
+                                </div>
+                                    
+
+                                <footer class="card-footer">
+                                    
+                                    <a href="#" class="card-footer-item">Edit/View</a>
+                                    
+                                </footer>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div> 
             </Fragment>
         );
     }
