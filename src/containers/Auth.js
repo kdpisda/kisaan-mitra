79536e9fec975393da1e812c40b1a312e4f80b6f
Amrita Mishra <amrita.mishra98@gmail.com>
2019-03-02 14:46:37 +0530

    update: auth page updated

--- src/containers/Auth.js
+++ src/containers/Auth.js
@@ -10,6 +10,41 @@ export default class Auth extends Component {
         return (
             <Fragment>
                 <Navbar/>
+                <div className="hero is-fullheight">
+                    <div className="hero-body has-text-centered">
+                        <div className="column is-one-third"></div>
+                        <div className="box">
+                            <div className="tabs is-centered">
+                                <ul>
+                                    <li class="is-active"><a>Login</a></li>
+                                    <li><a>Register</a></li>
+                                </ul>
+                            </div>
+                            <div className="field">
+                            <p className="control has-icons-left has-icons-right">
+                                <input className="input" type="username" placeholder="Username" />
+                                <span className="icon is-small is-left">
+                                <i className="fas fa-user"></i>
+                                </span>
+                            </p>
+                            </div>
+                            <div className="field">
+                            <p className="control has-icons-left">
+                                <input className="input" type="password" placeholder="Password" />
+                                <span className="icon is-small is-left">
+                                <i className="fas fa-lock"></i>
+                                </span>
+                            </p>
+                            </div>
+                            <div className="tabs is-centered">
+                                <ul>
+                                    <li><a>Don't have an Account?</a></li>
+                                    <li><a>Forgot Password?</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Fragment>
         );
     }
