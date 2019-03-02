707933a4013b5e9dfe95e40f07b9eecef2ae2750
Kuldeep Pisda <pisdak79@gmail.com>
2019-03-02 15:15:24 +0530

    update: added tabs

--- src/containers/Auth.js
+++ src/containers/Auth.js
@@ -5,6 +5,14 @@ import Navbar from "./components/Navbar";
 @inject("store")
 @observer
 export default class Auth extends Component {
+    state = {
+        loginTabActive : true
+    };
+
+    toggleTab(){
+        this.setState({loginTabActive: !this.state.loginTabActive});
+    }
+
     render() {
         const { ui } = this.props.store;
         return (
@@ -16,32 +24,80 @@ export default class Auth extends Component {
                         <div className="box">
                             <div className="tabs is-centered">
                                 <ul>
-                                    <li class="is-active"><a>Login</a></li>
-                                    <li><a>Register</a></li>
+                                    <li className={ this.state.loginTabActive
+                                                ? "is-active"
+                                                : ""
+                                            }
+                                    >
+                                        <a onClick={() => {
+                                            this.toggleTab();
+                                        }}>Login</a>
+                                    </li>
+                                    <li className={ !this.state.loginTabActive
+                                        ? "is-active"
+                                        : ""
+                                    }
+                                    >
+                                        <a onClick={() => {
+                                            this.toggleTab();
+                                        }}>Register</a>
+                                    </li>
                                 </ul>
                             </div>
-                            <div className="field">
-                            <p className="control has-icons-left has-icons-right">
-                                <input className="input" type="username" placeholder="Username" />
-                                <span className="icon is-small is-left">
-                                <i className="fas fa-user"></i>
-                                </span>
-                            </p>
-                            </div>
-                            <div className="field">
-                            <p className="control has-icons-left">
-                                <input className="input" type="password" placeholder="Password" />
-                                <span className="icon is-small is-left">
-                                <i className="fas fa-lock"></i>
-                                </span>
-                            </p>
-                            </div>
-                            <div className="tabs is-centered">
-                                <ul>
-                                    <li><a>Don't have an Account?</a></li>
-                                    <li><a>Forgot Password?</a></li>
-                                </ul>
-                            </div>
+                            {
+                                this.state.loginTabActive ? (
+                                    <div>
+                                        <div className="field">
+                                            <p className="control has-icons-left has-icons-right">
+                                                <input className="input" type="username" placeholder="Username" />
+                                                <span className="icon is-small is-left">
+                                            <i className="fas fa-user"></i>
+                                        </span>
+                                            </p>
+                                        </div>
+                                        <div className="field">
+                                            <p className="control has-icons-left">
+                                                <input className="input" type="password" placeholder="Password" />
+                                                <span className="icon is-small is-left">
+                                            <i className="fas fa-lock"></i>
+                                        </span>
+                                            </p>
+                                        </div>
+                                        <div className="tabs is-centered">
+                                            <ul>
+                                                <li><a>Don't have an Account?</a></li>
+                                                <li><a>Forgot Password?</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        Register
+                                        <div className="field">
+                                            <p className="control has-icons-left has-icons-right">
+                                                <input className="input" type="username" placeholder="Username" />
+                                                <span className="icon is-small is-left">
+                                            <i className="fas fa-user"></i>
+                                        </span>
+                                            </p>
+                                        </div>
+                                        <div className="field">
+                                            <p className="control has-icons-left">
+                                                <input className="input" type="password" placeholder="Password" />
+                                                <span className="icon is-small is-left">
+                                            <i className="fas fa-lock"></i>
+                                        </span>
+                                            </p>
+                                        </div>
+                                        <div className="tabs is-centered">
+                                            <ul>
+                                                <li><a>Don't have an Account?</a></li>
+                                                <li><a>Forgot Password?</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                )
+                            }
                         </div>
                     </div>
                 </div>
